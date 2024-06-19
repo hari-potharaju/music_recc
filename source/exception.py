6300f62e234c5b13296441ec9573466f8078d760
hari-potharaju <hpotharaju7@gmail.com>
2024-06-18 22:33:12 -0400

    final commit (logger + exception)

--- source/exception.py
+++ source/exception.py
@@ -1,19 +1,17 @@
 # Handles and logs exceptions to keep the program running smoothly and help with debugging.
 import sys 
+import logging
+import logger
+
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name= exc_tb.tb_frame.f_code.co_filename
     error_message="Error occued is python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
+        file_name,exc_tb.tb_lineno,str(error))
 
-    
-    )
+
     return error_message
-    
-    
-
-    
     
 
 
@@ -25,3 +23,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+if __name__ == "__main__":
+
+    try:
+        a=1/10
+    except Exception as e: 
+        logging.info("Divide by Zero")
+        raise CustomException(e, sys)
